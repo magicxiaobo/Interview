12d650d45ccd202ce781896ca9e5927899b08a43
magicxiaobo <yuxiaobohit@gmail.com>
2017-04-01 10:30:33 -0700

    Convert Sorted Array / List to BST

---
+++ ConvertSortedArrayToBST.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return buildTree(nums, 0, nums.length - 1);
+    }
+    public TreeNode buildTree(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildTree(nums, left, mid - 1);
+        root.right = buildTree(nums, mid + 1, right);
+        return root;
+    }
+}
---
+++ ConvertSortedListToBST.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        return buildTree(head, null);
+    }
+    public TreeNode buildTree(ListNode head, ListNode tail) {
+        if (head == tail) {
+            return null;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != tail && fast.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        root.left = buildTree(head, slow);
+        root.right = buildTree(slow.next, tail);
+        return root;
+    }
+}
